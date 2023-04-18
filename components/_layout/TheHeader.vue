fef9a32edd30e46db66852e588ddd21d1f5f3b16
Denis <zabavnikov.com@gmail.com>
2023-04-18 14:57:37 +0700

    поправил отступы в шапке.

--- components/_layout/TheHeader.vue
+++ components/_layout/TheHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="h-[48px] flex-shrink-0 flex items-center bg-white shadow-sm sticky top-0">
-    <NuxtLink to="/" class="flex items-center px-2 h-full">
+    <NuxtLink to="/" class="flex items-center px-4 h-full">
       <img src="/images/logotype.svg" alt="Trevio.ru - о путешествиях" class="w-auto h-[24px]" />
     </NuxtLink>
-    <NuxtLink v-if="! $auth.loggedIn" to="/login" class="flex items-center ml-auto px-2 h-full">Войти</NuxtLink>
+    <NuxtLink v-if="! $auth.loggedIn" to="/login" class="flex items-center ml-auto px-4 h-full">Войти</NuxtLink>
     <DropdownMenu v-else placement="bottom-end" class="ml-auto h-full">
       <template v-slot:default="{ isActive }">
-        <div class="flex items-center justify-center h-full px-2 overflow-hidden max-w-[180px]" :class="{'bg-slate-100': isActive}">
+        <div class="flex items-center justify-center h-full px-4 overflow-hidden max-w-[180px]" :class="{'bg-slate-100': isActive}">
           <img :src="$auth.user.avatar" :alt="$auth.user.name" class="w-8 h-8 rounded-full block" />
           <div class="truncate ml-2">{{ $auth.user.name }}</div>
         </div>
