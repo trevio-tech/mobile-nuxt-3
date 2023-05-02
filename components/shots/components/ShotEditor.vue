e2d25442c2f80d635d583cc83f479e0854e3eb51
Denis <zabavnikov.com@gmail.com>
2023-05-02 15:03:16 +0700

    wip

--- components/shots/components/ShotEditor.vue
+++ components/shots/components/ShotEditor.vue
@@ -1,40 +1,43 @@
 <template>
   <div class="flex flex-col items-end justify-end h-full w-full">
-    <button class="bg-black/50 rounded-lg fixed top-2 right-2 w-6 h-6 z-10" type="button" @click="overlay.hide">
-      <X class="text-white mx-auto w-5 h-5" />
+    <button class="bg-black/50 rounded-lg fixed top-4 right-4 w-8 h-8 z-10" type="button" @click="overlay.hide">
+      <X class="text-white mx-auto w-5 h-5"/>
     </button>
 
     <div class="fixed top-0 left-0 right-0 bottom-0">
       <canvas id="shot"></canvas>
     </div>
 
-    <ShotEditorTrashButton v-if="shotEditorStore.isTrash" id="trash" class="z-10 mx-auto mb-4" @mouseup="trashAdd"></ShotEditorTrashButton>
+    <ShotEditorTrashButton v-if="shotEditorStore.isTrash" id="trash" class="z-10 mx-auto mb-4"
+                           @mouseup="trashAdd"></ShotEditorTrashButton>
     <ShotEditorImageButton v-else @click="addImage" class="z-10 mx-auto mb-4"></ShotEditorImageButton>
 
     <footer class="relative z-10 p-2 w-full">
       <div class="bg-gray-50 w-full flex items-center rounded-lg">
-        <ShotEditorBackground class="w-[70%]" />
+        <ShotEditorBackground class="w-[70%]"/>
 
         <div class="border-l -ml-[1px] p-2 flex space-x-2">
-
-          <Button :loading="isLoading" class="flex-auto" @click="onSubmit(onSubmitCallback, form.isTravel)">Создать</Button>
+          <Button @click="showPanel = !showPanel" variant="secondary">
+            <Sliders class="w-5 h-5"/>
+          </Button>
+          <Button :loading="isLoading" class="flex-auto" @click="onSubmit(onSubmitCallback, form.isTravel)">Создать
+          </Button>
         </div>
       </div>
     </footer>
-    <!--      <div class="h-full flex flex-col">
 
-
-<div class="w-[320px] flex flex-col h-full bg-gray-100 rounded-tr-lg overflow-hidden">
-        <ul class="flex items-center cursor-pointer bg-slate-200 text-center font-medium text-sm border-b border-slate-300">
+    <section v-show="showPanel" ref="panel" class="fixed h-full flex flex-col z-10">
+      <div class="flex flex-col h-full bg-gray-100/80 backdrop-blur-sm rounded-tr-lg overflow-hidden">
+        <ul class="flex items-center cursor-pointer bg-slate-200 text-center font-medium text-sm border-b border-slate-200">
           <li @click="activeTab = 'text'" class="p-4 flex-1" :class="{'bg-slate-300': activeTab === 'text'}">Текст</li>
-          <li @click="activeTab = 'brushes'" class="p-4 flex-1" :class="{'bg-slate-300': activeTab === 'brushes'}">Кисти</li>
-          <li @click="activeTab = 'stickers'" class="p-4 flex-1" :class="{'bg-slate-300': activeTab === 'stickers'}">Стикеры</li>
+          <li @click="activeTab = 'brushes'" class="p-4 flex-1" :class="{'bg-slate-300': activeTab === 'brushes'}">
+            Кисти
+          </li>
         </ul>
 
         <div class="flex-auto flex flex-col">
-          <ShotEditorTextPanel v-show="activeTab === 'text'" />
-          <ShotEditorPanelBrushes  v-show="activeTab === 'brushes'" />
-          <ShotEditorStickerPanel v-show="activeTab === 'stickers'"/>
+          <ShotEditorTextPanel v-show="activeTab === 'text'"/>
+          <ShotEditorPanelBrushes v-show="activeTab === 'brushes'"/>
 
           <label v-if="lastUserTravel" for="last-travel" class="mt-auto text-sm p-4 ">
             <input type="checkbox" v-model="form.isTravel" id="last-travel">
@@ -42,20 +45,20 @@
           </label>
         </div>
       </div>
-    </div>-->
+    </section>
   </div>
 </template>
 
 <script setup>
 import ShotEditorBackground from '~/components/shots/components/ShotEditorBackground.vue'
 import ShotEditorImageButton from '~/components/shots/components/ShotEditorImageButton.vue'
-/*import ShotEditorStickerPanel from '~/components/shots/components/ShotEditorStickerPanel.vue'*/
 import ShotEditorTextPanel from '~/components/shots/components/ShotEditorTextPanel.vue'
 import ShotEditorTrashButton from '~/components/shots/components/ShotEditorTrashButton.vue'
 import ShotEditorPanelBrushes from '~/components/shots/components/ShotEditorPanelBrushes.vue'
-import { ref } from 'vue'
+import { ref, shallowRef } from 'vue'
 import { useShotsStore, useOverlay, Button, useShotEditor, usePageQuery } from '@trevio/ui'
-import { X } from 'lucide-vue-next'
+import { X, Sliders } from 'lucide-vue-next'
+import { useSwipe } from '@vueuse/core'
 
 const overlay = useOverlay()
 
@@ -70,11 +73,23 @@ const {
 
 const colors = ['white', 'black', 'red', 'green', 'yellow', 'orange']
 
-const activeTab = ref('text')
+const activeTab = shallowRef('text')
 const lastUserTravel = ref(null)
 
+const panel = ref(null)
+const showPanel = shallowRef(false)
+
+useSwipe(panel, {
+  onSwipeEnd(event, direction) {
+    if (direction === 'right') {
+      showPanel.value = false
+    }
+  },
+  threshold: 200
+})
+
 const form = ref({
-  isTravel: false
+  isTravel: false,
 })
 
 const onSubmitCallback = ({ createShot }) => {
@@ -96,11 +111,12 @@ try {
           title
         }
       }
-    `
+    `,
   })
 
   if (data.lastUserTravel.length) {
     lastUserTravel.value = data.lastUserTravel[0]
   }
-} catch (error) {}
+} catch (error) {
+}
 </script>
--- components/shots/components/ShotEditorBackground.vue
+++ components/shots/components/ShotEditorBackground.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex flex-wrap items-center overflow-x-auto space-x-2 p-2">
+  <div class="flex items-center overflow-hidden space-x-2 p-2">
     <div
         title="Случайный цвет фона"
         style="background-image: linear-gradient(rgb(218, 112, 165), rgb(253, 168, 31))"
-        class="w-6 h-6 rounded-lg cursor-pointer ring-2 ring-white flex items-center justify-center"
+        class="flex-shrink-0 w-6 h-6 rounded-lg cursor-pointer ring-2 ring-white flex items-center justify-center"
         @click="setRandomBackgroundGradient"
     >
       <Wand2 class="w-5 h-5 text-white"/>
@@ -11,17 +11,20 @@
     <div
         title="Добавить фоновое изображение"
         style="background-image: linear-gradient(rgb(118, 212, 165), rgb(153, 168, 31))"
-        class="w-6 h-6 rounded-lg cursor-pointer ring-2 ring-white flex items-center justify-center"
+        class="flex-shrink-0 w-6 h-6 rounded-lg cursor-pointer ring-2 ring-white flex items-center justify-center"
         @click="setBackgroundImage"
     >
       <ImagePlus class="w-5 h-5 text-white"/>
     </div>
-    <div
-      v-for="color in colors" :key="color"
-      :style="{ backgroundColor: color}"
-      class="w-6 h-6 rounded-lg cursor-pointer ring-2 ring-white"
-      @click="setBackgroundColor(color)"
-    >
+    <div class="flex items-center overflow-x-scroll space-x-2">
+      <div
+          v-for="color in colors" :key="color"
+          :style="{ backgroundColor: color}"
+          :class="{[color === 'white' ? 'border' : '']: true}"
+          class="flex-shrink-0 w-6 h-6 rounded-lg cursor-pointer ring-2 ring-white"
+          @click="setBackgroundColor(color)"
+      >
+      </div>
     </div>
   </div>
 </template>
