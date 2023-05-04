2f85fd6f6263a379cbbda241aa6ae79fcc0fc721
Denis <zabavnikov.com@gmail.com>
2023-05-04 17:20:09 +0700

    Добавил модалку создания фидбека.

--- components/_layout/TheHeader.vue
+++ components/_layout/TheHeader.vue
@@ -4,26 +4,43 @@
       <img src="/images/logotype.svg" alt="Trevio.ru - о путешествиях" class="w-auto h-[24px]" />
     </NuxtLink>
 
-    <div v-if="$auth.loggedIn" class="ml-auto flex items-center h-full">
-      <button @click="overlay.show(TheContentCreationDialog)" class="h-full focus:bg-slate-100 p-4 flex items-center">
-        <PlusCircle class="w-5 h-5" />
+    <div class="ml-auto flex items-center h-full">
+      <button @click="showFeedbackDialog" class="h-full focus:bg-slate-100 p-4 flex items-center">
+        <Bug class="w-5 h-5" />
       </button>
-      <div @click="$emit('update:userPanel', userPanel = !userPanel)" class="flex items-center justify-center ml-auto h-full px-4">
-        <img :src="$auth.user.avatar" :alt="$auth.user.name" class="w-8 h-8 rounded-full block" />
+      <div v-if="$auth.loggedIn" class="flex items-center h-full">
+        <button @click="showContentCreationDialog" class="h-full focus:bg-slate-100 p-4 flex items-center">
+          <PlusCircle class="w-5 h-5" />
+        </button>
+        <div @click="$emit('update:userPanel', userPanel = !userPanel)" class="flex items-center justify-center ml-auto h-full px-4">
+          <img :src="$auth.user.avatar" :alt="$auth.user.name" class="w-8 h-8 rounded-full block" />
+        </div>
       </div>
+      <NuxtLink v-else to="/login" class="flex items-center ml-auto px-4 h-full">Войти</NuxtLink>
     </div>
-    <NuxtLink v-else to="/login" class="flex items-center ml-auto px-4 h-full">Войти</NuxtLink>
   </header>
 </template>
 
 <script setup>
-import TheContentCreationDialog from './TheContentCreationDialog'
-import { DropdownMenu, DropdownMenuItem, useOverlay } from '@trevio/ui'
-import { PlusCircle } from 'lucide-vue-next'
+import { useOverlay } from '@trevio/ui'
+import { PlusCircle, Bug } from 'lucide-vue-next'
+import { defineAsyncComponent } from 'vue'
 
 defineProps({
   userPanel: Boolean
 })
 
 const overlay = useOverlay()
+
+const showContentCreationDialog = () => {
+  overlay.show(defineAsyncComponent(() => import('~/components/_layout/TheContentCreationDialog.vue')))
+}
+
+const showFeedbackDialog = () => {
+  overlay.show(defineAsyncComponent(() => import('~/components/feedback/components/FeedbackDialog.vue')), {
+    on: {
+      success: overlay.hide
+    }
+  })
+}
 </script>
--- components/feedback/components/FeedbackDialog.vue
+++ components/feedback/components/FeedbackDialog.vue
@@ -1,9 +1,91 @@
 <template>
-  <Dialog>
+  <Dialog title="Обратная связь">
+    <Form :submit="onSubmit" v-slot="{ isLoading }" class="space-y-4">
+      <FormField name="category_id" required label="Категория">
+        <label v-for="category in categories" :for="`complaint-category-${category.id}`" :key="category.id" class="flex items-center space-x-1">
+          <input type="radio" :id="`complaint-category-${category.id}`" name="category_id" :value="category.id" v-model="form.categoryId">
+          <span class="text-sm">{{ category.name }}</span>
+        </label>
+      </FormField>
 
+      <FormField name="email" label="Ваше имя" required v-slot="{ hasError }">
+        <Input v-model="form.name" maxlength="25" :has-error="hasError" />
+      </FormField>
+
+      <FormField name="email" label="Контактный email" required v-slot="{ hasError }">
+        <Input v-model="form.email" maxlength="35" :has-error="hasError" />
+      </FormField>
+
+      <FormField name="text" label="Комментарий" required v-slot="{ hasError }">
+        <Textarea v-model="form.text" maxlength="1000" :has-error="hasError" />
+      </FormField>
+
+      <Button class="w-full" type="submit" :loading="isLoading">Отправить</Button>
+    </Form>
   </Dialog>
 </template>
 
 <script setup>
+import 'vue3-toastify/dist/index.css'
 import Dialog from '~/components/_common/Dialog.vue'
+import { ref } from 'vue'
+import { toast } from 'vue3-toastify'
+import { useAuth } from '#imports'
+import { useQuery, Form, FormField, Button, Textarea, Input } from '@trevio/ui'
+
+const emits = defineEmits(['success'])
+const auth = useAuth()
+
+const categories = ref([])
+const form = ref({
+  categoryId: null,
+  email:      '',
+  name:       '',
+  text:       '',
+})
+
+if (auth.loggedIn) {
+  form.value.name = auth.user.name
+  form.value.email = auth.user.email
+}
+
+try {
+  const { data } = await useQuery({
+    query: `
+      query getFeedbackCategories {
+        feedbackCategories {
+          id
+          name
+        }
+      }
+    `
+  })
+
+  categories.value = data.feedbackCategories
+} catch (error) {
+  console.log(error)
+}
+
+const onSubmit = async () => {
+  const { data } = await useQuery({
+    query: /* GraphQL */`
+      mutation createFeedback($input: FeedbackInput!) {
+        createFeedback(input: $input)
+      }
+    `,
+    variables: {
+      input: { ...form.value }
+    }
+  })
+
+  if (data.createFeedback) {
+    toast.success('Отправлено!', {
+      position:   'top-center',
+      transition: 'slide',
+      autoClose:  1000,
+    })
+
+    emits('success')
+  }
+}
 </script>
